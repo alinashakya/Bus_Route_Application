d7a4648b361e3ddba3fd33c633f9c47eda0cb8f5
sarala <saralashrestha@lftechnology.com>
2013-10-03 11:50:01 +0545

    design integration

--- protected/views/busroute/create.php
+++ protected/views/busroute/create.php
@@ -1,49 +1,139 @@
-<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/media/js/jquery-1.9.1.min"></script>
-
-<p><?php echo CHtml::link("Check Out", array('../')); ?></p>
-
+<head>
 <?php
-if(isset($_GET) && !empty($_GET)){
-    $criteria = new CDbCriteria();
-    $criteria->condition = 'route_id ='.$_GET['key'];
-    $bus_route = BusStop::model()->findAll($criteria);
-}
-foreach ($bus_route as $value) { 
+$script = Yii::app()->clientScript;
+$script->registerCssFile(Yii::app()->request->baseUrl . '/media/css/themes/default/jquery.mobile-1.2.1.min.css');
+$script->registerCssFile(Yii::app()->request->baseUrl . '/media/css/style.css');
+$script->registerScriptFile(Yii::app()->request->baseUrl . '/media/js/jquery.js');
+$script->registerScriptFile(Yii::app()->request->baseUrl . '/media/js/jquery.mobile-1.2.1.min.js');
 ?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Leapfrog Bus Route</title>
+</head>
 
-    <?php $date = date('h:i', strtotime($value->time)); ?>
-    <div>
-        <ul>
+<body>
+<div class="conatiner" data-role="page">
+  <div data-role="header">
+    <header>
+      <div class="logo"> <a href="index.html">
+        <h1>Leapfrog Technology</h1>
+        </a> </div>
+      <!--end of logo-->
+      
+      <div class="menu"> <a class="home-icon" href="index.html">Home</a> </div>
+    </header>
+  </div>
+  <!-- /header -->
+  
+  <div class="content" data-role="content">
+  
+    <p><?php echo CHtml::link("Check Out", array('../')); ?></p>
+
+<section>
+     <div class="select-wrap">
+        <div class="select-content">
+          <p class="heading">Bhaktapur to Office</p>
+          <div class="sets places-wrap">
+            <?php
+            if(isset($_GET) && !empty($_GET)){
+            $criteria = new CDbCriteria();
+            $criteria->condition = 'route_id ='.$_GET['key'];
+            $bus_route = BusStop::model()->findAll($criteria);
+            }
+            foreach ($bus_route as $value) { 
+            ?>
+            <?php $date = date('h:i', strtotime($value->time)); ?>
+            <?php if(isset($value->created_time)){?>
+            <div class="place-checkin" id="<?php echo 'place_'.$value->id;?>" data-id="<?php echo $value->id;?>"> <a class="clickable-place oncheck " href="#">
+            <ul>
             <li>
                 <?php echo ucfirst($value->stop_name); ?>
                 <input type = "checkbox" class="chk" name="route_chk[]" id="<?php echo "route_chk_" . $value->id ?>" value="<?php echo $value->id;?>" <?php if(isset($value->created_time)){echo "checked='checked'";} ?>/>
             </li>
-        </ul>
-    </div>
+            </ul> 
+            </a>
+            </div>
+            <?php } else { ?>
+                <div class="place-checkin" id="<?php echo 'place_'.$value->id;?>" data-id="<?php echo $value->id;?>"> <a class="clickable-place default " href="#">
+            <ul>
+            <li>
+                <?php echo ucfirst($value->stop_name); ?>
+                <input type = "checkbox" class="chk" name="route_chk[]" id="<?php echo "route_chk_" . $value->id ?>" value="<?php echo $value->id;?>"/>
+            </li>
+            </ul> 
+            </a>
+            </div>
+            <?php } ?>
 
-<?php } ?>
+            <?php } ?>
+        </div>
+        </div>
+    </div>
+</section>
+       
+  
+  </div>
+  <!-- /content -->
+  
+  <div data-role="footer">
+    <footer>
+      <p>Copyright @ Leapfrog Technology.com</p>
+    </footer>
+  </div>
+  <!-- /header --> 
+  
+</div>
+<!-- /page -->
+</body>
+</html>
 
 <input type="hidden" id="url" value="<?php echo Yii::app()->request->baseUrl; ?>">
 <input type="hidden" id="bus_route" value="<?php echo $_GET['key']; ?>">
 
 <script type="text/javascript">
 
-    $(document).ready(function() {
-        var url = $('#url').val();
-        $('.chk').click(function() {
-            //var checked = $(this).val();
-            var checkbox = $(this);
-            if (checkbox.is(':checked')) {
-                var value = checkbox.val();
+var baseurl; 
+
+$(document).ready(function() {
+    baseurl = $('#url').val();
+    $('.place-checkin').click(function(){
+        var data_id = $(this).attr('data-id');
+        getValue(data_id);
+    });
+
+    $('.chk').click(function(){
+        var clicked_value = $(this).val();
+        if ($(this).is(':checked')){
+            $(this).prop('checked', false);
+        } else {
+            $(this).prop('checked', true);
+        }
+        getValue(clicked_value);
+        return false;
+    });
+
+   
+    
+});
+
+    function getValue(data_id){
+        var checkbox = $('#route_chk_'+data_id);
+        console.log(checkbox);
+            if (checkbox.is(':checked') == false) {
+
+                var value = $('#route_chk_'+data_id).val();
+                console.log(value);
                 var route_id = $('#bus_route').val();
                 $.ajax({
                     type: 'POST',
-                    url: url + '/busroute/addCheckedTime',
+                    url: baseurl + '/busroute/addCheckedTime',
                     data: {value: value,route_id:route_id},
                     dataType: 'json',
                     success: function(response) {
                         $.each(response, function(index, order) {
                             $('#route_chk_'+order).prop('checked',true);
+                            $('#place_'+order+' a').removeClass('default');
+                            $('#place_'+order+' a').addClass('oncheck');
                         });
                         
                     }
@@ -52,20 +142,20 @@ foreach ($bus_route as $value) {
                 var value = checkbox.val();
                 $.ajax({
                     type: 'POST',
-                    url: url + '/busroute/removeCheckedTime',
+                    url: baseurl + '/busroute/removeCheckedTime',
                     data: {value: value},
                     dataType: 'json',
                     success: function(response) {
                         $.each(response, function(index, order) {
                             $('#route_chk_'+order).prop('checked',false);
+                            $('#place_'+order+' a').removeClass('oncheck');
+                            $('#place_'+order+' a').addClass('default');
                         });
                         
                     }
                 });
             }
-
-        });
-    });
+    }
 
 
 </script>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,50 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+$script = Yii::app()->clientScript;
+$script->registerCssFile(Yii::app()->request->baseUrl . '/media/css/themes/default/jquery.mobile-1.2.1.min.css');
+$script->registerCssFile(Yii::app()->request->baseUrl . '/media/css/style.css');
+$script->registerScriptFile(Yii::app()->request->baseUrl . '/media/js/jquery.js');
+$script->registerScriptFile(Yii::app()->request->baseUrl . '/media/js/jquery.mobile-1.2.1.min.js');
+?>
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/media/css/jquery.mobile-1.4.0-alpha.2.min" />
-<script src="<?php echo Yii::app()->request->baseUrl; ?>/media/js/jquery-1.9.1.min"></script>
-<script src="<?php echo Yii::app()->request->baseUrl; ?>/media/js/jquery.mobile-1.4.0-alpha.2.min.js"></script>
+<title>Leapfrog Bus Route</title>
+<!--<link href="css/themes/default/jquery.mobile-1.2.1.min.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">-->
+<!--<link rel="stylesheet" href="docs/_assets/css/jqm-docs.css" />-->
 
+<!--<script src="js/jquery.js"></script>
+<script src="js/jquery.mobile-1.2.1.min.js"></script>-->
+</head>
+<body>
+<div class="container" data-role="page">
+  <div data-role="header">
+    <header>
+      <div class="logo">
+        <a href="index.html"> <h1>Leapfrog Technology</h1></a>
+      </div>
+      <!--end of logo--> 
+      
+    </header>
+  </div>
+  <!-- /header -->
+  
+  <div class="content" data-role="content">
+    <section>
+      <div class="select-wrap">
+        <div class="select-content">
+          <p class="heading">Select Bus Route - <?php echo Yii::app()->name;?></p>
+          <select>
+          <?php
+            $criteria = new CDbCriteria;
+            $criteria->condition = "status='1'";
+            $criteria->order = "route_name ASC";
 
-<div data-role="page">
+            $route = BusRoute::model()->findAll($criteria);
 
-    <div data-role="header">
-        <h1>Page Title</h1>
-    </div><!-- /header -->
-
-    <div data-role="content">
-        <p><?php $this->pageTitle = Yii::app()->name; ?></p>
-        <p><?php echo CHtml::link("Check In", array('confirm' => 'Please Select Route from the list'), array('class' => 'checkin')); ?></p>
-
-        <div data-role="fieldcontain">
-            <label for="select-native-17">Route:</label>
-            <select name="select-native-17" id="select-native-17">
-                <!--<option value="">Select One</option>-->
-                <?php
-                $criteria = new CDbCriteria;
-                $criteria->condition = "status='1'";
-                $criteria->order = "route_name ASC";
-                
-                $route = BusRoute::model()->findAll($criteria);
-
-                foreach ($route as $key => $value) {
-                    echo "<option value='" . $value->id . "'>$value->route_name ($value->bus_name)</option>";
-                }
-                ?>
-            </select>
-
-
+            foreach ($route as $key => $value) {
+                echo "<option value='" . $value->id . "'>$value->route_name ($value->bus_name)</option>";
+            }
+          ?>
+          </select>
         </div>
-        <div id="data">
-           
-
+        <!--end of select-content-->
+        
+        <div class="btn-wrap">
+          <!--<input type="button" value="Check In">-->
+          <!--<a href="checkin.html">Check In</a>-->
+          <?php echo CHtml::link("Check In", array('confirm' => 'Please Select Route from the list'), array('class' => 'checkin')); ?>
         </div>
-
-    </div><!-- /content -->
-
-    <div data-role="footer">
-        <h4>Page Footer</h4>
-    </div><!-- /footer -->
-</div><!-- /page -->
-
+      </div>
+      <!--end of select-wrap-->
+      
+      <div class="sets">
+        <ul class="places-display" data-role="listview" data-inset="true">
+          <li>Suryabinayak<em>8:00am</em></li>
+          <li>Thimi<em>8:10am</em></li>
+          <li>Koteshwor<em>8:200am</em></li>
+          <li>Item 4 <em>8:30am</em></li>
+          <li>Item 5</li>
+          <li>Item 6</li>
+        </ul>
+      </div>
+    </section>
+  </div>
+  <!-- /content -->
+  
+  <div data-role="footer">
+    <footer>
+      <p>Copyright @ Leapfrog Technology.com</p>
+    </footer>
+  </div>
+  <!-- /header --> 
+  
+</div>
+<!-- /page -->
+</body>
+</html>
 
 <input type="hidden" id="url" value="<?php echo Yii::app()->request->baseUrl; ?>">
 
@@ -81,7 +121,7 @@
                 dataType: 'html',
                 data: {value: selectedvalue},
                 success: function(data) {
-                    $('#data').html(data);
+                    $('.sets ul.places-display').html(data);
 
                 },
             });
@@ -94,28 +134,9 @@
             dataType: 'html',
             data: {value: selectedvalue},
             success: function(data) {
-                $('#data').html(data);
+                $('.sets ul.places-display').html(data);
             }
         });
     });
-
-//  var url = $('#url').val();
-//  $('select').change(function(){
-//      var selectedValue = $(':selected').val(); 
-//      $.ajax({
-//          type: 'POST',
-//          url: url+'/busroute/getList',
-//          data: {value:selectedValue},
-//          dataType: "json",
-//          success:function(response){
-//              if(response.value){
-//                  //console.log(response.value);
-//                  location.href = url+'/busroute/create';
-//
-//              }
-//          }
-//      });
-//  });
-
-
 </script>
+
