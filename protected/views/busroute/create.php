bd5dad363d0e2b3b77cca4648aab178b7794661e
sarala <saralashrestha@lftechnology.com>
2013-10-04 11:47:26 +0545

    changes

--- protected/views/busroute/create.php
+++ protected/views/busroute/create.php
@@ -1,118 +1,124 @@
 <section>
     <div class="btn-wrap checkout">
-        <?php echo CHtml::link("Check Out", array('../'),array('class' => 'checkin ui-link')); ?>
+        <?php echo CHtml::link("Check Out", array('../'), array('class' => 'checkin ui-link')); ?>
     </div>
     <?php
-    if(isset($_GET) && !empty($_GET)){
+    if (isset($_GET) && !empty($_GET)) {
         $bus_route = BusRoute::model()->findByAttributes(array('id' => $_GET['key']));
-    } ?>
+    }
+    ?>
     <div class="select-wrap">
         <div class="select-content">
-            <p class="heading"><?php echo $bus_route->route_name;?></p>
+            <p class="heading"><?php echo $bus_route->route_name; ?></p>
             <div class="sets places-wrap">
-            <p style="font-size:16px"><strong>Check In at your Stop</strong></p><br/>
-            <div><span id="loading" style="display:none"><img src="<?php echo Yii::app()->request->baseurl;?>/media/images/loading.gif"/></span></div>
-            <br/>
-            <?php
-            if(isset($_GET) && !empty($_GET)){
-                $criteria = new CDbCriteria();
-                $criteria->condition = 'route_id ='.$_GET['key'];
-                $bus_stop = BusStop::model()->findAll($criteria);
-            }
-            foreach ($bus_stop as $value) { 
-            ?>
-            <?php $date = date('h:i', strtotime($value->time)); ?>
-            <?php if(isset($value->created_time)) { ?>
-                <div class="place-checkin" id="<?php echo 'place_'.$value->id;?>" data-id="<?php echo $value->id;?>"> <a class="clickable-place oncheck " href="#"><em></em>
-                <ul>
-                    <li>
-                        <?php echo ucfirst($value->stop_name);?>
-                        <input type = "checkbox" class="chk" name="route_chk[]" id="<?php echo "route_chk_" . $value->id ?>" value="<?php echo $value->id;?>" <?php if(isset($value->created_time)){echo "checked='checked'";} ?>/>
-                    </li>
-                </ul> 
-                </a>
-                </div>
-            <?php } else { ?>
-                <div class="place-checkin" id="<?php echo 'place_'.$value->id;?>" data-id="<?php echo $value->id;?>"> <a class="clickable-place default " href="#">
-                <ul>
-                    <li>
-                        <?php echo ucfirst($value->stop_name); ?>
-                        <input type = "checkbox" class="chk" name="route_chk[]" id="<?php echo "route_chk_" . $value->id ?>" value="<?php echo $value->id;?>"/>
-                    </li>
-                </ul> 
-                </a>
-                </div>
-            <?php } ?>
-            <?php } ?>
+                <p style="font-size:16px"><strong>Check In at your Stop</strong></p><br/>
+                <?php date_default_timezone_set('Asia/Kathmandu'); ?>
+                <?php
+                if (isset($_GET) && !empty($_GET)) {
+                    $criteria = new CDbCriteria();
+                    $criteria->condition = 'route_id =' . $_GET['key'];
+                    $bus_stop = BusStop::model()->findAll($criteria);
+                }
+                foreach ($bus_stop as $value) {
+                    ?>
+                    <?php $date = date('h:i', strtotime($value->time)); ?>
+                    <?php if (isset($value->created_time)) { ?>
+                        <div class="place-checkin" id="<?php echo 'place_' . $value->id; ?>" data-id="<?php echo $value->id; ?>"> <a class="clickable-place oncheck " href="#">
+                                <ul>
+                                    <li>
+                                        <?php echo ucfirst($value->stop_name); ?>
+                                        <input type = "checkbox" class="chk" name="route_chk[]" id="<?php echo "route_chk_" . $value->id ?>" value="<?php echo $value->id; ?>" <?php
+                                        if (isset($value->created_time)) {
+                                            echo "checked='checked'";
+                                           // echo $value->created_time;
+                                        }
+                                        ?>/>
+                                        <em><?php echo date('h:i A',strtotime(time())); ?></em>
+                                    </li>
+                                </ul> 
+                            </a>
+                        </div>
+    <?php } else { ?>
+                        <div class="place-checkin" id="<?php echo 'place_' . $value->id; ?>" data-id="<?php echo $value->id; ?>"> <a class="clickable-place default " href="#">
+                                <ul>
+                                    <li>
+        <?php echo ucfirst($value->stop_name); ?>
+                                        <input type = "checkbox" class="chk" name="route_chk[]" id="<?php echo "route_chk_" . $value->id ?>" value="<?php echo $value->id; ?>"/>
+                                        <em><?php echo date('h:i A',strtotime(time())); ?></em>
+                                    </li>
+                                </ul> 
+                            </a>
+                        </div>
+    <?php } ?>
+<?php } ?>
             </div>
         </div>
     </div>
 </section>
-       
+
 <input type="hidden" id="url" value="<?php echo Yii::app()->request->baseUrl; ?>">
 <input type="hidden" id="bus_route" value="<?php echo $_GET['key']; ?>">
 
 <script type="text/javascript">
 
-var baseurl; 
+    var baseurl;
 
-$(document).ready(function() {
-    baseurl = $('#url').val();
-    $('.place-checkin').click(function(){
-        var data_id = $(this).attr('data-id');
-        $('#loading').css('display', '');
-        getValue(data_id);
+    $(document).ready(function() {
+        baseurl = $('#url').val();
+        $('.place-checkin').click(function() {
+            var data_id = $(this).attr('data-id');
+            getValue(data_id);
+        });
+
+        $('.chk').click(function() {
+            var clicked_value = $(this).val();
+            if ($(this).is(':checked')) {
+                $(this).prop('checked', false);
+            } else {
+                $(this).prop('checked', true);
+            }
+            getValue(clicked_value);
+            return false;
+        });
     });
 
-    $('.chk').click(function(){
-        var clicked_value = $(this).val();
-        $('#loading').css('display', '');
-        if ($(this).is(':checked')){
-            $(this).prop('checked', false);
+    function getValue(data_id) {
+        var checkbox = $('#route_chk_' + data_id);
+        if (checkbox.is(':checked') === false) {
+            var value = $('#route_chk_' + data_id).val();
+            //console.log(value);
+            var route_id = $('#bus_route').val();
+            var date_now = '<?php echo date('h:i A',strtotime(time()))//echo date('Y-m-d H:i:s'); ?>';
+            
+            $.ajax({
+                type: 'POST',
+                url: baseurl + '/busroute/addCheckedTime',
+                data: {value: value, route_id: route_id},
+                dataType: 'json',
+                success: function(response) {
+                    $.each(response, function(index, order) {
+                        $('#route_chk_' + order).prop('checked', true);
+                        $('#place_' + order + ' a').removeClass('default');
+                        $('#place_' + order + ' a').addClass('oncheck');
+                        $('#place_' + order + ' em').text(date_now);
+                    });
+                }
+            });
         } else {
-            $(this).prop('checked', true);
+            var value = checkbox.val();
+            $.ajax({
+                type: 'POST',
+                url: baseurl + '/busroute/removeCheckedTime',
+                data: {value: value},
+                dataType: 'json',
+                success: function(response) {
+                    $.each(response, function(index, order) {
+                        $('#route_chk_' + order).prop('checked', false);
+                        $('#place_' + order + ' a').removeClass('oncheck');
+                        $('#place_' + order + ' a').addClass('default');
+                    });
+                }
+            });
         }
-        getValue(clicked_value);
-        return false;
-    });
-});
-
-function getValue(data_id){
-    var checkbox = $('#route_chk_'+data_id);
-    if (checkbox.is(':checked') == false) {
-        var value = $('#route_chk_'+data_id).val();
-        //console.log(value);
-        var route_id = $('#bus_route').val();
-        $.ajax({
-            type: 'POST',
-            url: baseurl + '/busroute/addCheckedTime',
-            data: {value: value,route_id:route_id},
-            dataType: 'json',
-            success: function(response) {
-                $('#loading').css('display', 'none');
-                $.each(response, function(index, order) {
-                    $('#route_chk_'+order).prop('checked',true);
-                    $('#place_'+order+' a').removeClass('default');
-                    $('#place_'+order+' a').addClass('oncheck');
-                });
-            }
-        });
-    } else {
-        var value = checkbox.val();
-        $.ajax({
-            type: 'POST',
-            url: baseurl + '/busroute/removeCheckedTime',
-            data: {value: value},
-            dataType: 'json',
-            success: function(response) {
-                $('#loading').css('display', 'none');
-                $.each(response, function(index, order) {
-                    $('#route_chk_'+order).prop('checked',false);
-                    $('#place_'+order+' a').removeClass('oncheck');
-                    $('#place_'+order+' a').addClass('default');
-                });
-            }
-        });
     }
-}
 </script>
